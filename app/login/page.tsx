a74d275afc94354ecf762ab98a606e11b603c9d4
merhan-mohamed <merhan_mohamed89@hotmail.com>
2025-04-16 01:06:27 +0200

    update10

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,51 +1,66 @@
 import { Button, Checkbox, Label, TextInput } from "flowbite-react";
 import Link from "next/link";
 import { FcGoogle } from "react-icons/fc";
-import "../globals.css"
+import "../globals.css";
 import Footer from "@/components/Footer";
 
 const Login = () => {
   return (
     <>
-    <div className="container mt-20">
-     <form className="login mx-auto my-auto flex max-w-md flex-col gap-4">
-      <div>
-        <div className="mb-2 block">
-          <Label htmlFor="email2" value="Your Email" />
-        </div>
-        <TextInput id="email2" type="email" placeholder="name@hotmail.com" required shadow />
-      </div>
-      <div>
-        <div className="mb-2 block">
-          <Label htmlFor="password2" value="Your Password" />
-        </div>
-        <TextInput id="password2" type="password" required shadow />
-      </div>
-  
-     <div className="flex items-center justify-between">
-      <div className="flex items-center gap-2">
-        <Checkbox id="agree" />
-        <Label htmlFor="agree" className="flex text-sm">
-          Remember for 30 days
-        </Label>
-      </div>
-      <div>
-        <Link href="#" className="text-cyan-600 hover:underline dark:text-cyan-500 text-sm">
-            forget your password
-        </Link>
-      </div>
-      </div>
-   
-    
-      <Button type="submit">Sign in</Button>
-      <Button type="submit"><FcGoogle className="mr-2 text-xl" /> Sign in with Google</Button>
-     </form>
-     <h5 className="text-xs text-center mt-3">Don't have an account? <Link href="" className="text-cyan-600 hover:underline">sign up</Link></h5>
-    </div>
+      <div className="container mt-20">
+        <form className="login mx-auto my-auto flex max-w-md flex-col gap-4">
+          <div>
+            <div className="mb-2 block">
+              <Label htmlFor="email2" value="Your Email" />
+            </div>
+            <TextInput
+              id="email2"
+              type="email"
+              placeholder="name@hotmail.com"
+              required
+              shadow
+            />
+          </div>
+          <div>
+            <div className="mb-2 block">
+              <Label htmlFor="password2" value="Your Password" />
+            </div>
+            <TextInput id="password2" type="password" required shadow />
+          </div>
 
-    <Footer/>
+          <div className="flex items-center justify-between">
+            <div className="flex items-center gap-2">
+              <Checkbox id="agree" />
+              <Label htmlFor="agree" className="flex text-sm">
+                Remember for 30 days
+              </Label>
+            </div>
+            <div>
+              <Link
+                href="#"
+                className="text-sm text-cyan-600 hover:underline dark:text-cyan-500"
+              >
+                forget your password
+              </Link>
+            </div>
+          </div>
+
+          <Button type="submit">Sign in</Button>
+          <Button type="submit">
+            <FcGoogle className="mr-2 text-xl" /> Sign in with Google
+          </Button>
+        </form>
+        <h5 className="mt-3 text-center text-xs">
+          Don't have an account?{" "}
+          <Link href="" className="text-cyan-600 hover:underline">
+            sign up
+          </Link>
+        </h5>
+      </div>
+
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- app/payment/page.jsx
+++ app/payment/page.jsx
@@ -1,60 +1,77 @@
 "use client";
-import React, {useEffect} from 'react';
-import {useForm} from "react-hook-form";
+import React, { useEffect } from "react";
+import { useForm } from "react-hook-form";
 import { useDispatch, useSelector } from "react-redux";
-import {savePaymentMethod} from "@/redux/slices/cartSlice";
-import { useRouter } from 'next/navigation';
+import { savePaymentMethod } from "@/redux/slices/cartSlice";
+import { useRouter } from "next/navigation";
 import CheckWizard from "@/components/CheckWizard";
 import { Button, Label } from "flowbite-react";
+import Link from "next/link";
 
 const page = () => {
-    const {handleSubmit, setValue, register, formState:{errors}} = useForm()
-    const router = useRouter()
-    const dispatch = useDispatch()
-    const { shippingAddress, paymentMethod } = useSelector((state) => state.cart)
+  const {
+    handleSubmit,
+    setValue,
+    register,
+    formState: { errors },
+  } = useForm();
+  const router = useRouter();
+  const dispatch = useDispatch();
+  const { shippingAddress, paymentMethod } = useSelector((state) => state.cart);
 
-    const submitHandler = ({paymentMethod}) => {
-            dispatch(savePaymentMethod(paymentMethod))
-                router.push("/placeorder")
+  const submitHandler = ({ paymentMethod }) => {
+    dispatch(savePaymentMethod(paymentMethod));
+    router.push("/placeorder");
+  };
+
+  useEffect(() => {
+    if (!shippingAddress.address) {
+      return router.push("/shipping");
     }
-
-    useEffect(() => {
-        if(!shippingAddress.address){
-            return router.push("/shipping")
-        }
-        setValue("paymentMethod", paymentMethod)
-    }, [paymentMethod]) 
+    setValue("paymentMethod", paymentMethod);
+  }, [paymentMethod]);
 
   return (
-<div>
-    <CheckWizard activeStep={2}/>
-    <form
-       className='mx-auto max-w-screen-md'
-       onSubmit={handleSubmit(submitHandler)}>
-        <h1 className='mb-10 mt-8 text-xl underline font-bold'>Payment Method</h1>
-        {['Visa', "Fawry", "CashOnDelivery"].map((payment) => (
-            <div className='mb-4' key={payment}>
-                    <input 
-                     type='radio'
-                     value={payment} 
-                     name="paymentMethod" 
-                     className='outline-none focus:ring-0' 
-                     id={payment}
-                     {...register("paymentMethod", {
-                        required: " please select payment method"
-                     })}
-                     />
-                      <Label htmlFor={payment} value={payment} className='ml-2'/>
-                     
-            </div> 
-        ))}
-         {errors.paymentMethod && (<div className='text-red-500 mb-3'>{errors.paymentMethod.message}</div>)}
-    <div className='mb-4 flex justify-between'>
-        <Button color="dark" type='Submit'>Next</Button>
-    </div>
-    </form>
-</div>
-  )
-}
+    <div>
+      <CheckWizard activeStep={2} />
+      <form
+        className="mx-auto max-w-screen-md"
+        onSubmit={handleSubmit(submitHandler)}
+      >
+        <h1 className="mb-10 mt-8 text-xl font-bold underline">
+          Payment Method
+        </h1>
+        {["Visa"].map((payment) => (
+          <div className="mb-4" key={payment}>
+            <input
+              type="radio"
+              value={payment}
+              name="paymentMethod"
+              className="outline-none focus:ring-0"
+              id={payment}
+              {...register("paymentMethod", {
+                required: " please select payment method",
+              })}
+            />
 
-export default page
+            <Label htmlFor={payment} value={payment} className="ml-2" />
+          </div>
+        ))}
+        {errors.paymentMethod && (
+          <div className="mb-3 text-red-500">
+            {errors.paymentMethod.message}
+          </div>
+        )}
+        <div className="mb-4 flex justify-between">
+          <Link href="/Stripe">
+            <Button color="dark" type="Submit">
+              Next
+            </Button>
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default page;
